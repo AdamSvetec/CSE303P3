42964eed2400ba4ee0214aeccda1117b11370a9a
AdamSvetec <adamsvetec@gmail.com>
2016-09-28 18:52:20 -0400

    WIP: initial versions of malloc and free in interop      -still need to implement functions in list.c

--- libpart2.c
+++ libpart2.c
@@ -1,4 +1,8 @@
 #include <stdint.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#include <stdio.h>
 
 /*
  * We aren't providing much code here.  You'll need to implement your own
@@ -16,3 +20,20 @@ int map_count();
 void map_dump();
 
 /* TODO: Your code goes here */
+
+/* interop malloc */
+void * malloc(size_t size){
+  void * (*realmalloc)(size_t)=(void*(*)(size_t))(dlsym (RTLD_NEXT, "malloc"));
+  void * malloced_ptr = realmalloc(size);
+  map_insert((uintptr_t)malloced_ptr, "TODO", "TODO");
+  printf("malloc\n");
+  return malloced_ptr;
+}
+
+/* interop free */
+void free(void *ptr){
+  printf("free\n");
+  void (*realfree)(void *ptr)=(void(*)(void * ptr))(dlsym (RTLD_NEXT, "free"));
+  realfree(ptr);
+  map_remove((uintptr_t)ptr);
+}
