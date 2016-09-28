726400485c9d67045852b69514a3535c5b70a8fa
AdamSvetec <adamsvetec@gmail.com>
2016-09-28 16:43:40 -0400

    Add team members

--- team.c
+++ team.c
@@ -4,8 +4,8 @@
  * Please be sure to fill in this struct right away!
  */
 struct team_t team = {
-  "", /* first member name, e.g., Jason Loew */
-  "", /* first member email, e.g., jal715@lehigh.edu */
-  "", /* second member name, e.g., Michael Spear */
-  ""  /* second member email, e.g., mfs409@lehigh.edu */
+  "Adam Svetec", /* first member name, e.g., Jason Loew */
+  "ajs217@lehigh.edu", /* first member email, e.g., jal715@lehigh.edu */
+  "Mark Schatzman", /* second member name, e.g., Michael Spear */
+  "mss517@lehigh.edu"  /* second member email, e.g., mfs409@lehigh.edu */
 };
