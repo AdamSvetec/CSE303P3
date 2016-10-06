6db10ae1ac256c8da9d6c6760dfcb5677675f1d5
AdamSvetec <adamsvetec@gmail.com>
2016-10-06 14:10:21 -0400

    add function to search for scanf or printf in backtrace

--- libpart4.c
+++ libpart4.c
@@ -3,17 +3,25 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
 /*
- * We aren't providing much code here.  You'll need to implement your own
- * dlsym(), as well as any constructors or destructors for your library.
+  parse backtrace and look for printf or scanf calls
  */
-
-
 int check_backtrace(char ** strings, int length){
-  return 1;
+  for(int i = 0; i < length; i++){
+    if(strstr(strings[i], "printf") != NULL || strstr(strings[i], "scanf") != NULL){
+      return 1;
+    }
+  }
+  return 0;
 }
 
+/*
+ interpositioned pipe
+ -if call comes from printf or scanf, make this function unexecutable
+ */
 int pipe(int pipefd[2]){
 
   int (*realpipe)(int pipefd[2])=(int(*)(int pipefd[2]))(dlsym (RTLD_NEXT, "pipe"));
@@ -28,7 +36,7 @@ int pipe(int pipefd[2]){
     void * pipe_func_ptr = pipe;
     int pagesize=sysconf(_SC_PAGE_SIZE);
     uintptr_t space_past_pagesize= (uintptr_t)pipe_func_ptr % (uintptr_t)pagesize;
-    mprotect((void*)((uintptr_t)pipe_func_ptr - space_past_pagesize), (size_t)(space_past_pagesize+2), PROT_EXEC);
+    mprotect((void*)((uintptr_t)pipe_func_ptr - space_past_pagesize), (size_t)(space_past_pagesize), PROT_NONE);
   }
 
   return realpipe(pipefd);  
